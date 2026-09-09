95162aeb0bb1d1ba9a7679e834c09f60c5847068
lindo <492201845@qq.com>
2026-09-09 17:31:03 +0800

    fix: correct TypeXSharedPrefsPath macro expansion; bump version to 2.7.44

--- common.h
+++ common.h
@@ -130,7 +130,7 @@
 #define secondActionDelay 0.05
 
 #define TypeXCachePath DX_ROOT_PATH_NS(@"/var/mobile/Library/Caches/com.lindo.typex")
-#define TypeXSharedPrefsPath TypeXCachePath @"/shared.plist"
+#define TypeXSharedPrefsPath DX_ROOT_PATH_NS(@"/var/mobile/Library/Caches/com.lindo.typex/shared.plist")
 
 static inline NSString *DXScopedPreferenceKey(NSString *baseKey, NSString *configuration) {
     if ([configuration isEqualToString:@"top"]) {
